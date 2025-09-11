76a5a603ac51825232914d1c7761ffd64549e203
SatoryKono <821311@gmail.com>
2025-09-11 12:51:52 +0300

    1

--- run_all.bat
+++
@@ -1,5 +0,0 @@
->python get_input_initialisation.py 
---same-doc data\input\ChEMBL\ChEMBL_same_document_20_05.xlsx 
---all-doc data\input\ChEMBL\ChEMBL_all_10_05_step5.xlsx 
---dictionary-dir dictionary 
---out-dir data\input
