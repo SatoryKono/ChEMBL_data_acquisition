c1eb920e03f10621b48380305be7038cc58b9084
SatoryKono <821311@gmail.com>
2025-09-12 23:21:28 +0300

    Handle duplicate activity columns before concatenation

--- library/input_initialisation_library.py
+++ library/input_initialisation_library.py
@@ -1052,7 +1052,9 @@ def build_combined_tables(
 
     # --- activity --------------------------------------------------------
     df_same_act = unify_dtypes(same["activity"])
+    df_same_act = df_same_act.loc[:, ~df_same_act.columns.duplicated()]
     df_all_act = unify_dtypes(all_["activity"])
+    df_all_act = df_all_act.loc[:, ~df_all_act.columns.duplicated()]
     concat = pd.concat([df_same_act, df_all_act], ignore_index=True, sort=False)
     concat = concat.drop(
         columns=list(ACTIVITY_DROP_COLS & set(concat.columns)), errors="ignore"
