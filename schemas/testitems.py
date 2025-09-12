e2fbded84517670085f76cc5ce3f16b6c84cc346
SatoryKono <821311@gmail.com>
2025-09-12 11:55:53 +0300

    1

--- schemas/testitems.py
+++ schemas/testitems.py
@@ -7,6 +7,7 @@ import pandera.pandas as pa
 TestitemsSchema: pa.DataFrameSchema = pa.DataFrameSchema(
     {
         "salt_chembl_id": pa.Column(str, required=True),
+        "salt_chembl_id": pa.Column(str, required=False),
         "molecule_chembl_id": pa.Column(str, required=True),
         "molecule_type": pa.Column(
             str,
@@ -16,6 +17,7 @@ TestitemsSchema: pa.DataFrameSchema = pa.DataFrameSchema(
             required=True,
         ),
         "chirality": pa.Column(int, pa.Check.isin([-1, 0, 1, 2]), required=False),
+        "mw_freebase": pa.Column(float, pa.Check.in_range(0, 2000), required=False),
         "num_ro5_violations": pa.Column(float, pa.Check.in_range(0, 5), required=False),
         "is_radical": pa.Column(bool, required=False),
     }
