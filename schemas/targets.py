75c2311fe09e39910bdf5852c3ad04e6bf920779
SatoryKono <821311@gmail.com>
2025-09-12 21:37:25 +0300

    1

--- schemas/targets.py
+++ schemas/targets.py
@@ -10,9 +10,7 @@ TargetsSchema: pa.DataFrameSchema = pa.DataFrameSchema(
         # ``organism`` is not present in all datasets; treat as optional to allow
         # validation of partial records without skipping the entire step.
         "organism": pa.Column(str, required=False),
-        "target_uniprot_id": pa.Column(str, required=False),
-        "pH_dependence": pa.Column(float, pa.Check.in_range(0, 14), required=False),
-        "isoforms": pa.Column(float, pa.Check.ge(0), required=False),
+        "uniprot_id": pa.Column(str, required=False),        
     }
 )
 
