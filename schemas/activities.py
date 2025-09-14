085cb9c22115544597909a07c544b5e57cfb9382
SatoryKono <821311@gmail.com>
2025-09-14 15:57:52 +0300

    1

--- schemas/activities.py
+++ schemas/activities.py
@@ -6,12 +6,17 @@ expected structure of activity dataframes.
 
 from __future__ import annotations
 
+from typing import cast
+
 import pandera.pandas as pa
+from pandera.dtypes import DataType
+
+PA_ANY = cast(DataType, None)
 
 # Definition of the schema describing the activities table.
 ActivitiesSchema: pa.DataFrameSchema = pa.DataFrameSchema(
     {
-        "activity_id": pa.Column(object, required=True, nullable=True),
+        "activity_id": pa.Column(PA_ANY, required=True, nullable=True),
         "molecule_chembl_id": pa.Column(str, required=True, nullable=True),
         "assay_chembl_id": pa.Column(str, required=True, nullable=True),
         "activity_comment": pa.Column(str, required=False, nullable=True),
@@ -24,12 +29,12 @@ ActivitiesSchema: pa.DataFrameSchema = pa.DataFrameSchema(
         "data_validity_description": pa.Column(str, required=False, nullable=True),
         "document_chembl_id": pa.Column(str, required=False, nullable=True),
         "pchembl_value": pa.Column(object, required=False, nullable=True),
-        "potential_duplicate": pa.Column(str, required=False, nullable=True),
+        "potential_duplicate": pa.Column(PA_ANY, required=False, nullable=True),
         "qudt_units": pa.Column(str, required=False, nullable=True),
-        "record_id": pa.Column(str, required=False, nullable=True),
-        "relation": pa.Column(str, required=False, nullable=True),
-        "src_assay_id": pa.Column(object, required=False, nullable=True),
-        "src_id": pa.Column(object, required=False, nullable=True),
+        "record_id": pa.Column(PA_ANY, required=False, nullable=True),
+        "relation": pa.Column(object, required=False, nullable=True),
+        "src_assay_id": pa.Column(PA_ANY, required=False, nullable=True),
+        "src_id": pa.Column(PA_ANY, required=False, nullable=True),
         "standard_relation": pa.Column(str, required=False, nullable=True),
         "standard_units": pa.Column(str, required=False, nullable=True),
         "type": pa.Column(str, required=False, nullable=True),
