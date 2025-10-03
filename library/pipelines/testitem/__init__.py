31706994cac124b3503628d0e4d888ebf865a540
SatoryKono <821311@gmail.com>
2025-10-03 09:23:15 +0300

    Fix PubChem compatibility exports for test item pipeline

--- library/pipelines/testitem/__init__.py
+++ library/pipelines/testitem/__init__.py
@@ -8,13 +8,23 @@ Changelog
 from __future__ import annotations
 
 # ===== Modules =====
+from importlib import import_module
+from importlib.util import find_spec
+
 from .enrichment import enrich
+from library.pipelines.assay.chembl_assay import TESTITEM_PUBCHEM_COLUMNS
+
 
 # ===== Compatibility Exports =====
-from library.testitem_pipeline.pubchem import (
-    PUBCHEM_CID_CACHE_ENCODING,
-    PUBCHEM_COLUMNS,
-)
+_PUBCHEM_COMPAT_MODULE = "library.testitem_pipeline.pubchem"
+
+if find_spec(_PUBCHEM_COMPAT_MODULE) is not None:
+    pubchem_module = import_module(_PUBCHEM_COMPAT_MODULE)
+    PUBCHEM_CID_CACHE_ENCODING = pubchem_module.PUBCHEM_CID_CACHE_ENCODING
+    PUBCHEM_COLUMNS = list(pubchem_module.PUBCHEM_COLUMNS)
+else:
+    PUBCHEM_CID_CACHE_ENCODING = "utf-8"
+    PUBCHEM_COLUMNS = list(TESTITEM_PUBCHEM_COLUMNS)
 
 __all__ = [
     "enrich",
