687294bdc0597896fa872794adebb1d01070fcaa
SatoryKono <821311@gmail.com>
2025-10-03 11:22:05 +0300

    Fix optional export collection guard

--- library/testitem_pipeline/__init__.py
+++ library/testitem_pipeline/__init__.py
@@ -263,9 +263,11 @@ _EXTRA_EXPORTS = (
     "write_meta_yaml",
 )
 
+_COLLECTED_EXTRAS = globals().get("_EXTRA_EXPORTS", ())
+
 __all__ = _collect_exports(
     _CATALOG_EXPORTS,
     _CLI_EXPORTS,
     _PUBCHEM_EXPORTS,
-    extras=_EXTRA_EXPORTS,
+    extras=_COLLECTED_EXTRAS,
 )
