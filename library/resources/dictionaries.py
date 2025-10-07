702ab9a8a60fdc6320766d648d2d5384acf341aa
SatoryKono <821311@gmail.com>
2025-10-07 12:36:43 +0300

    Handle additional dictionary checksum variants

--- library/resources/dictionaries.py
+++ library/resources/dictionaries.py
@@ -48,6 +48,10 @@ _KNOWN_CHECKSUM_VARIANTS: Mapping[str, tuple[str, ...]] = {
         "efc69f6bb252d68bc7fde11ba98b09b24b0b8fd868fcd6d945eaca76b636f43a",
         "ac67acf2dcd801ffbe9d6e3aa95189af7c3e991fb3ddaaf8aab0be988d7d3224",
     ),
+    "target_uniprot_cache": (
+        "014e183b12959a4e5f060faf3b77c6a6d143cc00e0dd0121fdd1d1e51a210a2a",
+        "c86b314b5d8a0906f1174c8e9f494cf9dde6841be2cb1e8b66c5772976afb5ca",
+    ),
 }
 
 _ENV_CHECKSUM_ALLOWLIST = "CHEMBL_DICTIONARY_CHECKSUM_ALLOWLIST"
