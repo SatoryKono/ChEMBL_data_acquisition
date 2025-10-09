2572d09eac7c0420990b317f79e14f7136164057
SatoryKono <821311@gmail.com>
2025-10-09 09:45:07 +0300

    Fix postprocessing CLI imports when run as scripts

--- scripts/make_testitem_postprocessing.py
+++ scripts/make_testitem_postprocessing.py
@@ -27,19 +27,34 @@ from library.postprocess.common.types import SchemaValidationError, StepError
 from library.postprocess.testitem.schema import TESTITEM_SCHEMA, validate_testitems
 from library.postprocess.testitem.steps import run_testitem_pipeline
 
-from ._postprocess_common import (
-    CsvRuntimeConfig,
-    DEFAULT_LOG_DIR,
-    LOG_DIR_ENV,
-    export_postprocess_frame,
-    generate_metrics_report,
-    get_csv_runtime_config,
-    get_default_log_level,
-    get_pipeline_config,
-    load_input_frame,
-    run_postprocess_steps,
-    validate_postprocess_frame,
-)
+if __package__ in {None, ""}:
+    from _postprocess_common import (
+        CsvRuntimeConfig,
+        DEFAULT_LOG_DIR,
+        LOG_DIR_ENV,
+        export_postprocess_frame,
+        generate_metrics_report,
+        get_csv_runtime_config,
+        get_default_log_level,
+        get_pipeline_config,
+        load_input_frame,
+        run_postprocess_steps,
+        validate_postprocess_frame,
+    )
+else:  # pragma: no cover - executed when imported as a package module
+    from ._postprocess_common import (
+        CsvRuntimeConfig,
+        DEFAULT_LOG_DIR,
+        LOG_DIR_ENV,
+        export_postprocess_frame,
+        generate_metrics_report,
+        get_csv_runtime_config,
+        get_default_log_level,
+        get_pipeline_config,
+        load_input_frame,
+        run_postprocess_steps,
+        validate_postprocess_frame,
+    )
 
 
 TABLE_NAME = "testitems"
