20eb34b6bb522e821ea0eb71678f73afca38880c
SatoryKono <821311@gmail.com>
2025-11-29 14:58:51 +0300

    feat(pubmed): add PubMedDataClientImpl and factory/normalizer

--- src/bioetl/clients/factories/__init__.py
+++ src/bioetl/clients/factories/__init__.py
@@ -2,9 +2,11 @@
 
 from .client_factory import ClientFactory
 from .helpers import build_normalizer, build_transport
+from .pubmed_client_factory_impl import PubMedClientFactoryImpl
 
 __all__ = [
     "ClientFactory",
+    "PubMedClientFactoryImpl",
     "build_normalizer",
     "build_transport",
 ]
---
+++ src/bioetl/clients/providers/pubmed/__init__.py
@@ -0,0 +1,4 @@
+from .pubmed_data_client_impl import PubMedDataClientImpl
+from .pubmed_normalizer_impl import PubMedNormalizerImpl
+
+__all__ = ["PubMedDataClientImpl", "PubMedNormalizerImpl"]
