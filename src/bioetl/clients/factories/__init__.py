91c3e860c86a6860a18b4d4e74ab4f9d62ed8839
SatoryKono <821311@gmail.com>
2025-11-29 14:57:31 +0300

    feat(pubchem): add PubChemDataClientImpl and factory/normalizer

--- src/bioetl/clients/factories/__init__.py
+++ src/bioetl/clients/factories/__init__.py
@@ -2,9 +2,11 @@
 
 from .client_factory import ClientFactory
 from .helpers import build_normalizer, build_transport
+from .pubchem_client_factory_impl import PubChemClientFactoryImpl
 
 __all__ = [
     "ClientFactory",
     "build_normalizer",
     "build_transport",
+    "PubChemClientFactoryImpl",
 ]
